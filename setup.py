370f95608027d738ebf34d123f4adba8b6504e56
Msaad-786 <msaad.bee20seecs@seecs.edu.pk>
2025-01-23 16:06:27 +0800

    Files Updated

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 #installing local package in my current Virtualenv
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Muhammad Saad',
+    author_email='m.saad44490@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
+
